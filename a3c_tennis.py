5026dc51b31f312f3402f1756d02bd2b40ed672e
feng.chen <fengchen@kuang-chi.com>
2017-07-25 17:16:52 +0800

    init

---
+++ a3c_tennis.py
@@ -0,0 +1,353 @@
+# coding: utf-8
+
+# # Simple Reinforcement Learning with Tensorflow Part 8: Asynchronus Advantage Actor-Critic (A3C)
+#
+# This iPython notebook includes an implementation of the [A3C algorithm](https://arxiv.org/pdf/1602.01783.pdf). In it we use A3C to solve a simple 3D Doom challenge using the [VizDoom engine](http://vizdoom.cs.put.edu.pl/). For more information on A3C, see the accompanying [Medium post](https://medium.com/p/c88f72a5e9f2/edit).
+#
+# This tutorial requires that VizDoom is installed. It can be easily obtained with:
+#
+# `pip install vizdoom`
+#
+# We also require `basic.wad` and `helper.py`, both of which are available from the [DeepRL-Agents github repo](https://github.com/awjuliani/DeepRL-Agents).
+#
+# While training is taking place, statistics on agent performance are available from Tensorboard. To launch it use:
+#
+# `tensorboard --logdir=worker_0:'./train_0',worker_1:'./train_1',worker_2:'./train_2',worker_3:'./train_3'`
+
+# In[ ]:
+
+import gym
+import os
+import threading
+import multiprocessing
+import skimage
+from skimage import transform, color, exposure
+import numpy as np
+# import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import scipy.signal
+# get_ipython().magic('matplotlib inline')
+from time import sleep
+import logging.handlers
+
+# Copies one set of variables to another.
+# Used to set worker network parameters to those of global network.
+def update_target_graph(from_scope, to_scope):
+    from_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, from_scope)
+    to_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, to_scope)
+
+    op_holder = []
+    for from_var, to_var in zip(from_vars, to_vars):
+        op_holder.append(to_var.assign(from_var))
+    return op_holder
+
+# # Processes Doom screen image to produce cropped and resized image.
+# def process_frame(frame):
+#     s = frame[10:-10, 30:-30]
+#     s = scipy.misc.imresize(s, [84, 84])
+#     s = np.reshape(s, [np.prod(s.shape)]) / 255.0
+#     return s
+
+# preprocess the screen image
+def preprocess(processed_input):
+    processed_output = processed_input[30:250, 8:160]  # crop
+    processed_output = skimage.color.rgb2gray(processed_output)
+    processed_output = skimage.transform.resize(processed_output,(84,84))
+    processed_output = skimage.exposure.rescale_intensity(processed_output,out_range=(0,255))
+    processed_output = np.reshape(processed_output, [np.prod(processed_output.shape)])
+    return processed_output # 84*84
+
+# Discounting function used to calculate discounted returns.
+def discount(x, gamma):
+    return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+
+
+# Used to initialize weights for policy and value output layers
+def normalized_columns_initializer(std=1.0):
+    def _initializer(shape, dtype=None, partition_info=None):
+        out = np.random.randn(*shape).astype(np.float32)
+        out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
+        return tf.constant(out)
+
+    return _initializer
+
+
+# ### Actor-Critic Network
+
+# In[ ]:
+
+class AC_Network():
+    def __init__(self, s_size, a_size, scope, trainer):
+        with tf.variable_scope(scope):
+            # Input and visual encoding layers
+            self.inputs = tf.placeholder(shape=[None, s_size], dtype=tf.float32)
+            self.imageIn = tf.reshape(self.inputs, shape=[-1, 84, 84, 1])
+            self.conv1 = slim.conv2d(activation_fn=tf.nn.elu,
+                                     inputs=self.imageIn, num_outputs=16,
+                                     kernel_size=[8, 8], stride=[4, 4], padding='VALID')
+            self.conv2 = slim.conv2d(activation_fn=tf.nn.elu,
+                                     inputs=self.conv1, num_outputs=32,
+                                     kernel_size=[4, 4], stride=[2, 2], padding='VALID')
+            hidden = slim.fully_connected(slim.flatten(self.conv2), 256, activation_fn=tf.nn.elu)
+
+            # Recurrent network for temporal dependencies
+            lstm_cell = tf.contrib.rnn.BasicLSTMCell(256, state_is_tuple=True)
+            c_init = np.zeros((1, lstm_cell.state_size.c), np.float32)
+            h_init = np.zeros((1, lstm_cell.state_size.h), np.float32)
+            self.state_init = [c_init, h_init]
+            c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
+            h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
+            self.state_in = (c_in, h_in)
+            rnn_in = tf.expand_dims(hidden, [0])
+            step_size = tf.shape(self.imageIn)[:1]
+            state_in = tf.contrib.rnn.LSTMStateTuple(c_in, h_in)
+            lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
+                lstm_cell, rnn_in, initial_state=state_in, sequence_length=step_size,
+                time_major=False)
+            lstm_c, lstm_h = lstm_state
+            self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
+            rnn_out = tf.reshape(lstm_outputs, [-1, 256])
+
+            # Output layers for policy and value estimations
+            self.policy = slim.fully_connected(rnn_out, a_size,
+                                               activation_fn=tf.nn.softmax,
+                                               weights_initializer=normalized_columns_initializer(0.01),
+                                               biases_initializer=None)
+            self.value = slim.fully_connected(rnn_out, 1,
+                                              activation_fn=None,
+                                              weights_initializer=normalized_columns_initializer(1.0),
+                                              biases_initializer=None)
+
+            # Only the worker network need ops for loss functions and gradient updating.
+            if scope != 'global':
+                self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
+                self.actions_onehot = tf.one_hot(self.actions, a_size, dtype=tf.float32)
+                self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
+                self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
+
+                self.responsible_outputs = tf.reduce_sum(self.policy * self.actions_onehot, [1])
+
+                # Loss functions
+                self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value, [-1])))
+                self.entropy = - tf.reduce_sum(self.policy * tf.log(self.policy))
+                self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs) * self.advantages)
+                self.loss = 0.5 * self.value_loss + self.policy_loss - self.entropy * 0.01
+
+                # Get gradients from local network using local losses
+                local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+                self.gradients = tf.gradients(self.loss, local_vars)
+                self.var_norms = tf.global_norm(local_vars)
+                grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, 40.0)
+
+                # Apply local gradients to global network
+                global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
+                self.apply_grads = trainer.apply_gradients(zip(grads, global_vars))
+
+
+class Worker():
+    def __init__(self, name, s_size, a_size, trainer, model_path, global_episodes):
+        self.name = "worker_" + str(name)
+        self.number = name
+        self.model_path = model_path
+        self.trainer = trainer
+        self.global_episodes = global_episodes
+        self.increment = self.global_episodes.assign_add(1)
+        self.episode_rewards = []
+        self.episode_lengths = []
+        self.episode_mean_values = []
+        self.summary_writer = tf.summary.FileWriter(SUMMARY_PATH + str(self.number))
+
+        # Create the local copy of the network and the tensorflow op to copy global parameters to local network
+        self.local_AC = AC_Network(s_size, a_size, self.name, trainer)
+        self.update_local_ops = update_target_graph('global', self.name)
+
+        self.actions = np.identity(a_size, dtype=bool).tolist()
+
+        self.env = gym.make("Tennis-v0")
+
+    def train(self, rollout, sess, gamma, bootstrap_value):
+        rollout = np.array(rollout)
+        observations = rollout[:, 0]
+        actions = rollout[:, 1]
+        rewards = rollout[:, 2]
+        next_observations = rollout[:, 3]
+        values = rollout[:, 5]
+
+        # Here we take the rewards and values from the rollout, and use them to
+        # generate the advantage and discounted returns.
+        # The advantage function uses "Generalized Advantage Estimation"
+        self.rewards_plus = np.asarray(rewards.tolist() + [bootstrap_value])
+        discounted_rewards = discount(self.rewards_plus, gamma)[:-1]
+        self.value_plus = np.asarray(values.tolist() + [bootstrap_value])
+        advantages = rewards + gamma * self.value_plus[1:] - self.value_plus[:-1]
+        advantages = discount(advantages, gamma)
+
+        # Update the global network using gradients from loss
+        # Generate network statistics to periodically save
+        rnn_state = self.local_AC.state_init
+        feed_dict = {self.local_AC.target_v: discounted_rewards,
+                     self.local_AC.inputs: np.vstack(observations),
+                     self.local_AC.actions: actions,
+                     self.local_AC.advantages: advantages,
+                     self.local_AC.state_in[0]: rnn_state[0],
+                     self.local_AC.state_in[1]: rnn_state[1]}
+        v_l, p_l, e_l, g_n, v_n, _ = sess.run([self.local_AC.value_loss,
+                                               self.local_AC.policy_loss,
+                                               self.local_AC.entropy,
+                                               self.local_AC.grad_norms,
+                                               self.local_AC.var_norms,
+                                               self.local_AC.apply_grads],
+                                              feed_dict=feed_dict)
+        return v_l / len(rollout), p_l / len(rollout), e_l / len(rollout), g_n, v_n
+
+    def map_action_from_label_to_env(self, from_act):
+        to_act = from_act
+        if from_act == 0:
+            to_act = 1
+        elif (from_act >= 1 and from_act < 9):
+            to_act = from_act + 9
+        return to_act
+
+    def sample(self, distribution):
+        act = np.random.choice(a_size, p=distribution)
+        env_act = self.map_action_from_label_to_env(act)
+        return act, env_act
+
+    def work(self, max_episode_length, gamma, sess, coord, saver):
+        episode_count = sess.run(self.global_episodes)
+        total_steps = 0
+        reward_sum = 0
+        logger.warn("Starting worker %d" % (self.number))
+        done = True
+        with sess.as_default(), sess.graph.as_default():
+            while not coord.should_stop():
+                sess.run(self.update_local_ops)
+                episode_buffer = []
+                episode_values = []
+                episode_reward = 0
+                episode_step_count = 0
+                if done:
+                    observation = self.env.reset()
+                    state = preprocess(observation)
+                    done = False
+                rnn_state = self.local_AC.state_init
+                reward = 0.0
+                while (reward == 0.0):
+                    # self.env.render()
+                    # Take an action using probabilities from policy network output.
+                    a_dist, value, rnn_state = sess.run(
+                        [self.local_AC.policy, self.local_AC.value, self.local_AC.state_out],
+                        feed_dict={self.local_AC.inputs: [state],
+                                   self.local_AC.state_in[0]: rnn_state[0],
+                                   self.local_AC.state_in[1]: rnn_state[1]})
+                    action, env_action = self.sample(a_dist[0])
+                    observation, reward, done, info = self.env.step(env_action)
+                    state1 = preprocess(observation)
+                    episode_buffer.append([state, action, reward, state1, done, value[0, 0]])
+                    episode_values.append(value[0, 0])
+                    episode_reward += reward
+                    # logger.debug('action: %d, %d, reward: %d, done: %r' % (action, env_action, reward, done))
+                    reward_sum += reward
+                    total_steps += 1
+                    episode_step_count += 1
+                    state = state1
+
+                self.episode_rewards.append(episode_reward)
+                self.episode_lengths.append(episode_step_count)
+                self.episode_mean_values.append(np.mean(episode_values))
+
+                # Update the network using the experience buffer at the end of the episode.
+                if len(episode_buffer) != 0:
+                    v_l, p_l, e_l, g_n, v_n = self.train(episode_buffer, sess, gamma, 0.0)
+
+                # Periodically save gifs of episodes, model parameters, and summary statistics.
+                # if episode_count % 5 == 0 and episode_count != 0:
+                if episode_count % EPISODE_BATCH_SIZE == (EPISODE_BATCH_SIZE - 1):
+                    mean_reward = np.mean(self.episode_rewards[-5:])
+                    mean_length = np.mean(self.episode_lengths[-5:])
+                    mean_value = np.mean(self.episode_mean_values[-5:])
+                    summary = tf.Summary()
+                    summary.value.add(tag='Perf/Reward', simple_value=float(mean_reward))
+                    summary.value.add(tag='Perf/Length', simple_value=float(mean_length))
+                    summary.value.add(tag='Perf/Value', simple_value=float(mean_value))
+                    summary.value.add(tag='Losses/Value Loss', simple_value=float(v_l))
+                    summary.value.add(tag='Losses/Policy Loss', simple_value=float(p_l))
+                    summary.value.add(tag='Losses/Entropy', simple_value=float(e_l))
+                    summary.value.add(tag='Losses/Grad Norm', simple_value=float(g_n))
+                    summary.value.add(tag='Losses/Var Norm', simple_value=float(v_n))
+                    self.summary_writer.add_summary(summary, episode_count)
+                    self.summary_writer.flush()
+                    logger.warn('episode %d, average reward %f' % (episode_count, reward_sum / EPISODE_BATCH_SIZE))
+                    if reward_sum > 0:
+                        logger.warn('Task solved in %d episodes!' % (episode_count))
+                    reward_sum = 0
+
+                if episode_count % SAVE_INTERVAL == (SAVE_INTERVAL - 1) and self.name == 'worker_0':
+                    logger.warn('save model at epoch %d' % (episode_count))
+                    ckpt_file = os.path.join(self.model_path, 'a3c_tennis')
+                    saver.save(sess, ckpt_file, (episode_count + 1) / SAVE_INTERVAL)
+
+                if self.name == 'worker_0':
+                    sess.run(self.increment)
+                episode_count += 1
+
+
+max_episode_length = 300
+gamma = .99  # discount rate for advantage estimation and reward discounting
+s_size = 7056  # Observations are greyscale frames of 84 * 84 * 1
+a_size = 9  # Agent can move Left, Right, or Fire
+load_model = False
+MODEL_PATH = './model'
+SUMMARY_PATH = './summary/train_'
+LOG_PATH = './log'
+MAX_LOG_SIZE = 2560000
+LOG_BACKUP_NUM = 4000
+EPISODE_BATCH_SIZE = 100
+SAVE_INTERVAL = 10000
+
+logger = logging.getLogger('a3c_tennis')
+log_file = os.path.join(LOG_PATH, 'a3c_tennis.log')
+handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=MAX_LOG_SIZE, backupCount=LOG_BACKUP_NUM)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+tf.reset_default_graph()
+
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
+
+with tf.device("/cpu:0"):
+    global_episodes = tf.Variable(0, dtype=tf.int32, name='global_episodes', trainable=False)
+    trainer = tf.train.AdamOptimizer(learning_rate=1e-4)
+    master_network = AC_Network(s_size, a_size, 'global', None)  # Generate global network
+    num_workers = multiprocessing.cpu_count()  # Set workers ot number of available CPU threads
+    # num_workers = 1
+    workers = []
+    # Create worker classes
+    for i in range(num_workers):
+        workers.append(Worker(i, s_size, a_size, trainer, MODEL_PATH, global_episodes))
+    saver = tf.train.Saver(max_to_keep=5)
+
+with tf.Session() as sess:
+    coord = tf.train.Coordinator()
+    if load_model == True:
+        logger.warn ('Loading Model...')
+        ckpt = tf.train.get_checkpoint_state(MODEL_PATH)
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        sess.run(tf.global_variables_initializer())
+
+    # This is where the asynchronous magic happens.
+    # Start the "work" process for each worker in a separate threat.
+    worker_threads = []
+    for worker in workers:
+        worker_work = lambda: worker.work(max_episode_length, gamma, sess, coord, saver)
+        t = threading.Thread(target=(worker_work))
+        t.start()
+        sleep(0.5)
+        worker_threads.append(t)
+    coord.join(worker_threads)
+
